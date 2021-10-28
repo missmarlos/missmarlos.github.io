5e6dad4ad885d0626a546b8fb0966e39391e2d79
Marela Carlos <marelacarlos@Marelas-MBP.hitronhub.home>
2021-10-28 12:39:59 -0600

    Added portfolio for 581 Project 2

--- src/Main/App.tsx
+++ src/Main/App.tsx
@@ -5,6 +5,7 @@ import text from '../text.json';
 import {HashRouter as Router, Switch, Route} from 'react-router-dom';
 import Barkipedia from '../Portfolio/Barkipedia/Barkipedia';
 import Hypertrophy from '../Portfolio/Hypertrophy/Hypertrophy';
+import PhoneUnlock from '../Portfolio/PhoneUnlock/PhoneUnlock';
 
 function App() {
   var page = text[0].content.text ?? "";
@@ -18,6 +19,7 @@ function App() {
             <Route path="/home" >
               <Home text={page}/>
             </Route>
+            <Route path="/portfolio4" component={PhoneUnlock}/>
             <Route path="/portfolio3" component={MeButton}/>
             <Route path="/portfolio2" component={Barkipedia}/>
             <Route path="/portfolio1" component={Hypertrophy}/>
---
+++ src/Portfolio/PhoneUnlock/Sketches.tsx
@@ -0,0 +1,236 @@
+import i1 from '../../Assets/PhoneUnlock/InitialSketches/1.png';
+import i2 from '../../Assets/PhoneUnlock/InitialSketches/2.png';
+import i3 from '../../Assets/PhoneUnlock/InitialSketches/3.png';
+import i4 from '../../Assets/PhoneUnlock/InitialSketches/4.png';
+import i5 from '../../Assets/PhoneUnlock/InitialSketches/5.png';
+import i6 from '../../Assets/PhoneUnlock/InitialSketches/6.png';
+import i7 from '../../Assets/PhoneUnlock/InitialSketches/7.png';
+
+import m1 from '../../Assets/PhoneUnlock/RefinedSketches/m1.png';
+import m2 from '../../Assets/PhoneUnlock/RefinedSketches/m2.png';
+import m3 from '../../Assets/PhoneUnlock/RefinedSketches/m3.png';
+import m4 from '../../Assets/PhoneUnlock/RefinedSketches/m4.png';
+import m5 from '../../Assets/PhoneUnlock/RefinedSketches/m5.png';
+import m6 from '../../Assets/PhoneUnlock/RefinedSketches/m6.png';
+import m7 from '../../Assets/PhoneUnlock/RefinedSketches/m7.png';
+import m8 from '../../Assets/PhoneUnlock/RefinedSketches/m8.png';
+import m9 from '../../Assets/PhoneUnlock/RefinedSketches/m9.png';
+import m10 from '../../Assets/PhoneUnlock/RefinedSketches/m10.png';
+
+import s1 from '../../Assets/PhoneUnlock/RefinedSketches/s1.png';
+import s2 from '../../Assets/PhoneUnlock/RefinedSketches/s2.png';
+import s3 from '../../Assets/PhoneUnlock/RefinedSketches/s3.png';
+import s4 from '../../Assets/PhoneUnlock/RefinedSketches/s4.png';
+import s5 from '../../Assets/PhoneUnlock/RefinedSketches/s5.png';
+import s6 from '../../Assets/PhoneUnlock/RefinedSketches/s6.png';
+import s7 from '../../Assets/PhoneUnlock/RefinedSketches/s7.png';
+import s8 from '../../Assets/PhoneUnlock/RefinedSketches/s8.png';
+import s9 from '../../Assets/PhoneUnlock/RefinedSketches/s9.png';
+import s10 from '../../Assets/PhoneUnlock/RefinedSketches/s10.png';
+
+import d1 from '../../Assets/PhoneUnlock/RefinedSketches/d1.png';
+import d2 from '../../Assets/PhoneUnlock/RefinedSketches/d2.png';
+import d3 from '../../Assets/PhoneUnlock/RefinedSketches/d3.png';
+import d4 from '../../Assets/PhoneUnlock/RefinedSketches/d4.png';
+import d5 from '../../Assets/PhoneUnlock/RefinedSketches/d5.png';
+import d6 from '../../Assets/PhoneUnlock/RefinedSketches/d6.png';
+import d7 from '../../Assets/PhoneUnlock/RefinedSketches/d7.png';
+import d8 from '../../Assets/PhoneUnlock/RefinedSketches/d8.png';
+import d9 from '../../Assets/PhoneUnlock/RefinedSketches/d9.png';
+import d10 from '../../Assets/PhoneUnlock/RefinedSketches/d10.png';
+
+
+
+
+export const conceptSketches = [
+    {
+        image: i1,
+        title: "Part A - Marela",
+        description: "Choose a subset of images to be your password."
+    },
+    {
+        image: i2,
+        title: "Part A - Sophia",
+        description: "The phone only unlocks when Pikachu has his ketchup bottle in hand. Drag and drop the ketchup bottle from the table to Pikachu to unlock the phone."
+    },
+    {
+        image: i3,
+        title: "Part A - Sophia",
+        description: "Your avatar is initially in a “bad mood”. In order to increase their happiness and unlock food, drag and drop the “correct” food dishes from the conveyor belt onto your avatar."
+    },
+    {
+        image: i4,
+        title: "Part A - Daniel",
+        description: "Hold down the spin button while you shape the clay into a pot with your right fingers."
+    },
+    {
+        image: i5,
+        title: "Part B - Marela",
+        description: "Tap the mic and the morse code animations appear on the screen."
+    },
+    {
+        image: i6,
+        title: "Part B - Sophia",
+        description: "Starts with a cold Gudetama wrapped in a bacon blanket. The user is to place their hand over the proximity sensor and apply slight pressure to the screen. Gudetama then warms up and the phone unlocks."
+    },
+    {
+        image: i7,
+        title: "Part B - Daniel",
+        description: "Use NFC/Bluetooth sensor to unlock phone. Use Heartrate to unlock phone."
+    },
+]
+
+export const refinedSketches = [
+    {
+        image: m1,
+        title: "Part A - Marela",
+        description: "Flick unwanted items away."
+    },
+    {
+        image: m2,
+        title: "Part A - Marela",
+        description: "Arrange items in a specific order akin to a scrapbook full of stickers."
+    },
+    {
+        image: m3,
+        title: "Part A - Marela",
+        description: "Stack food on plate in a specific order."
+    },
+    {
+        image: m4,
+        title: "Part A - Marela",
+        description: "Arrange items on the table."
+    },
+    {
+        image: m5,
+        title: "Part A - Marela",
+        description: "Pottery multi-touch. Draw the left side of the heart with one finger and draw the right side of the heart with three fingers simultaneously."
+    },
+    {
+        image: m6,
+        title: "Part B - Marela",
+        description: "Bake cookies by using your hand to control the temperature of the oven."
+    },
+    {
+        image: m7,
+        title: "Part B - Marela",
+        description: "Gudetama wants a hot coffee, not an iced coffee. Place your hand at a certain distance away from the phone. The built-in proximity sensor measures this distance and the phone will unlock once the correct distance is detected."
+    },
+    {
+        image: m8,
+        title: "Part B - Marela",
+        description: "Tap paddcode word out in morse code and the character's alarm will wake them up."
+    },
+    {
+        image: m9,
+        title: "Part B - Marela",
+        description: "User must do some activity to get their heart reate to reach an active range."
+    },
+    {
+        image: m10,
+        title: "Part B - Marela",
+        description: "Tap passcode word out in morse code and the egg will crack into the pan. Use the proximity sensor to cook the egg."
+    },
+
+    {
+        image: s1,
+        title: "Part A - Sophia",
+        description: "Choose a subset of images to be your password. For added security, the order in which images are chosen matters. If the password is not completed after a certain time limit, the screen resets and a new set of items is presented to the user. Another alternative, it to correctly select the ingredients of a secret dish item."
+    },
+    {
+        image: s2,
+        title: "Part A - Sophia",
+        description: "A tree of images that grows an extra two nodes every time the correct image is selected."
+    },
+    {
+        image: s3,
+        title: "Part A - Sophia",
+        description: "Big Yoshi is low health and needs to be healed. Drag and drop a Maximum Tomato onto Yoshi to unlock the phone."
+    },
+    {
+        image: s4,
+        title: "Part A - Sophia",
+        description: "Your puppy is in a bad mood and requires a certain item to be happy again. Drag and drop the correct item onto the puppy to unlock the phone."
+    },
+    {
+        image: s5,
+        title: "Part A - Sophia",
+        description: "Start off by using your pointer finger to stir the soup. Then, use your other hand to tap the screen and add some crackers to the soup with each tap. To add a security aspect, you can stir a bowl of Alphagetti soup until the password completely forms in the soup."
+    },
+    {
+        image: s6,
+        title: "Part B - Sophia",
+        description: "Place your hand over the phone to warm up the penguin. The hand must cover the proximity sensor and a slight amount of pressure must be applied to the screen for the warming to take affect. Once the penguin is warm enough, the phone will unlock."
+    },
+    {
+        image: s7,
+        title: "Part B - Sophia",
+        description: "The snowman is slowly melting away. Cover the built-in proximity sensor with your hand for three seconds to protect him from the blazing sun. The snowman will then stop melting and the phone will unlock."
+    },
+    {
+        image: s8,
+        title: "Part B - Sophia",
+        description: "Your avatar is walking at a constant speed along a straight path. Tap the proximity sensor with your finger to jump and hit the number blocks floating above your head. Hit the correct number blocks to unlock your phone."
+    },
+    {
+        image: s9,
+        title: "Part B - Sophia",
+        description: "A variation of the whack-a-mole game where the proximity sensor must be tapped on for Yoshi to whack a mole."
+    },
+    {
+        image: s10,
+        title: "Part B - Sophia",
+        description: "A character prompts you to increase your heartbeat to unlock your phone with the heart rate sensor."
+    },
+
+    {
+        image: d1,
+        title: "Part A - Daniel",
+        description: "Select multiple items and link with drag."
+    },
+    {
+        image: d2,
+        title: "Part A - Daniel",
+        description: "Select items and throw them to your avatar. The correct item must be chosen to unlock the phone."
+    },
+    {
+        image: d3,
+        title: "Part A - Daniel",
+        description: "Sequential selection of items."
+    },
+    {
+        image: d4,
+        title: "Part A - Daniel",
+        description: "Forge the sword to unlock."
+    },
+    {
+        image: d5,
+        title: "Part A - Daniel",
+        description: "Stretch and cut."
+    },
+    {
+        image: d6,
+        title: "Part B - Daniel",
+        description: "A chilly, little bird is taking a peaceful nap. Shake the phone to abruptly wake it up. Then pet it to warm it up and ultimately unlock the phone."
+    },
+    {
+        image: d7,
+        title: "Part B - Daniel",
+        description: "Tap to the beat of a song to unlock."
+    },
+    {
+        image: d8,
+        title: "Part B - Daniel",
+        description: "Shake the bubble tea."
+    },
+    {
+        image: d9,
+        title: "Part B - Daniel",
+        description: "Using the compass sensor, face that direction and then tap your passcode onto the microphone."
+    },
+    {
+        image: d10,
+        title: "Part B - Daniel",
+        description: "Take your fingerprint and detect heartbeat simultaneously."
+    }
+]
